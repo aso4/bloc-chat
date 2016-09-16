d7d1f518b9ea5497d93f13c88439d2376257b2fb
aso4 <albertyso@gmail.com>
2016-09-15 23:53:25 -0400

    phil's modifications due to checkpoint ambiguity

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,28 +1,28 @@
- (function() {
-     function config($stateProvider, $locationProvider) {
-         $locationProvider
-             .html5Mode({
-                 enabled: true,
-                 requireBase: false
-             });
-         $stateProvider
-             .state('home', {
-                 url: '/',
-                 templateUrl: '/templates/home.html'
-             });
-//             .state('album', {
-//                 url: '/album',
-//                 controller: 'AlbumCtrl as album',
-//                 templateUrl: '/templates/album.html'
-//             })
-//             .state('collection', {
-//                 url: '/collection',
-//                 controller: 'CollectionCtrl as collection',
-//                 templateUrl: '/templates/collection.html'
-//             });
-     }
- 
-     angular
-         .module('blocChat', ['ui.router'])
-         .config(config);
- })();
+//  (function() {
+//      function config($stateProvider, $locationProvider) {
+//          $locationProvider
+//              .html5Mode({
+//                  enabled: true,
+//                  requireBase: false
+//              });
+//          $stateProvider
+//              .state('home', {
+//                  url: '/home',
+//                  templateUrl: '/templates/home.html'
+//              });
+// //             .state('album', {
+// //                 url: '/album',
+// //                 controller: 'AlbumCtrl as album',
+// //                 templateUrl: '/templates/album.html'
+// //             })
+// //             .state('collection', {
+// //                 url: '/collection',
+// //                 controller: 'CollectionCtrl as collection',
+// //                 templateUrl: '/templates/collection.html'
+// //             });
+//      }
+//
+//      angular
+//          .module('blocChat', ['ui.router'])
+//          .config(config);
+//  })();
